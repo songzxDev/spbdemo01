7060ccd169ed35b84f98aad3a3f30fece835d399
songzxDev <songzx_2326@163.com>
2019-07-24 12:08:26 +0800

    增加前端访问页面，并配置热部署

---
+++ src/main/java/org/songzx/spbdemo01/controller/PageController.java
@@ -0,0 +1,13 @@
+package org.songzx.spbdemo01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/pageBase/")
+    public String pageBase() {
+        return "base.html";
+    }
+}
